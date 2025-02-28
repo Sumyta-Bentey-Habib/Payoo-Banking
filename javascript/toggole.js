d2f7fa6805b4f4189cd481c3412116a36bba7f8d
Your Name <you@example.com>
2025-02-28 15:59:08 +0600

    final

--- javascript/toggole.js
+++ javascript/toggole.js
@@ -1,12 +1,14 @@
 document.getElementById("cash-out").style.display = "none";
 
 
+
 document.getElementById("add-money-box").addEventListener("click", function() {
     document.getElementById("cash-out").style.display = "none";
-    document.getElementById("add-money").style.display = "block";
+    document.getElementById("addmoney").style.display = "block";
 });
 
 document.getElementById("cash-out-box").addEventListener("click", function() { 
+    document.getElementById("addmoney").style.display = "none";
     document.getElementById("cash-out").style.display = "block";
-    document.getElementById("add-money").style.display = "none"; 
+   
 });
